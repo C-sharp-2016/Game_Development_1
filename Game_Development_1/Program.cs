a3ea1a1eabf8c65e04e1eee96953abf6942b73c9
Jesus Erwin Suarez <mrjesuserwinsuarez@gmail.com>
2016-01-20 08:21:23 +0800

    ask for name and print

---
+++ Game_Development_1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_Development_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare variable
+            string name;
+
+            // Ask to enter name as "string"
+            Console.WriteLine("Please enter your name");
+            name = Console.ReadLine();
+
+            // Print name entered
+            Console.WriteLine("Your name is " + name);
+            Console.ReadLine();
+        }
+    }
+}
